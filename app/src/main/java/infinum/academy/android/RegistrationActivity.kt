0ee64ad99dcfca44c97df1d0dc8ff24aa805a2a2
zavrskina <petra.zavrski@infinum.hr>
2022-07-21 13:01:47 +0200

    add bottom sheet dialog

--- app/src/main/java/infinum/academy/android/RegistrationActivity.kt
+++ app/src/main/java/infinum/academy/android/RegistrationActivity.kt
@@ -1,11 +1,12 @@
 package infinum.academy.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import infinum.academy.android.databinding.ActivityRegistrationBinding
+import infinum.academy.android.databinding.DialogRegistrationStateBinding
 import infinum.academy.android.networking.ApiModule
 
 class RegistrationActivity : AppCompatActivity() {
@@ -31,5 +32,30 @@ class RegistrationActivity : AppCompatActivity() {
                 password = passwordEditText.text.toString()
             )
         }
+    }
+
+    fun displayRegistrationMessage(isSuccessful: Boolean) {
+        val dialog = BottomSheetDialog(this, R.style.AppBottomSheetDialogTheme)
+        val bottomSheetBinding = DialogRegistrationStateBinding.inflate(layoutInflater)
+
+        if (isSuccessful) {
+            bottomSheetBinding.registrationStatePicture.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this,
+                    R.drawable.ic_outline_check_circle_24
+                )
+            )
+        } else {
+            bottomSheetBinding.registrationStatePicture.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this,
+                    R.drawable.ic_outline_do_not_disturb_alt_24
+                )
+            )
+        }
+
+        dialog.setContentView(bottomSheetBinding.root)
+        dialog.show()
+
     }
 }
