17de992e0b19093a17104c8e8fd5b3052aa00f60
zavrskina <petra.zavrski@infinum.hr>
2022-07-27 20:27:16 +0200

    add uplload of photos

--- app/src/main/java/infinum/academy/android/AcademyInfoActivity.kt
+++ app/src/main/java/infinum/academy/android/AcademyInfoActivity.kt
@@ -2,6 +2,7 @@ package infinum.academy.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import infinum.academy.android.databinding.ActivityAcademyInfoBinding
 
 class AcademyInfoActivity : AppCompatActivity() {
@@ -15,4 +16,25 @@ class AcademyInfoActivity : AppCompatActivity() {
 
         setContentView(binding.root)
     }
+
+//    fun setLecturerImages(countryCode: Int) = with(binding) {
+//        when(countryCode) {
+//            Country.CROATIA.countryCode -> {
+//                lecturerOneImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.turic_renato))
+//                lecturerTwoImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.floreani_filip))
+//            }
+//            Country.SLOVENIA.countryCode -> {
+//                lecturerOneImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.frelih_mark))
+//                lecturerTwoImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.valic_david))
+//            }
+//            Country.MACEDONIA.countryCode -> {
+//                lecturerOneImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.stojanovski_filip))
+//                lecturerTwoImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.toshev_ivan))
+//            }
+//            Country.MONTENEGRO.countryCode -> {
+//                lecturerOneImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.rakonjac_mladen))
+//                lecturerTwoImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.mugosa_balsa))
+//            }
+//        }
+//    }
 }
